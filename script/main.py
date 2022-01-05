4ec8d783fa0ff4185d36cc630a07c976ee550c9e
Yusuke Yamasaki <yamasaki@hogehage.com>
2022-01-05 15:29:53 +0000

    Add comments

--- script/main.py
+++ script/main.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-# Subscriber
+'''
+その数字を割り切ることが出来る素数を判定して、結果を出力していきたい。
+
+素数：2, 3, 5, 7, 11, 13, 17, 19, 23, 29, ・・・
+
+課題：serviceで使用するmessageの型に、リストを使えるのかどうか(数値ならいけることはわかっている。文字でいけるかが問題)（文字でいけなくても数値で判定してまた別の手段で置き換えてやればいい）
+'''
 
 import rospy
 from std_msgs.msg import Int32
@@ -28,7 +34,7 @@ def main():
         check_b = int(n)
         check_res = check(check_b)
 
-        print("   "+str(n)+"  | "+check_res.result)
+        print("   "+str(n)+"  | "+check_res.result) # -3だけ出力させないようにしたほうが良さげ
         rate.sleep()
 
 
